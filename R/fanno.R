b17f588c1b9c5f1085520b4165c052ac1ecde42d
agalecki <agalecki@umich.edu>
2017-07-28 13:25:42 -0400

    Update fanno.R

--- R/fanno.R
+++ R/fanno.R
@@ -7,6 +7,7 @@
    fun  <- getx[["objs"]][nx]
    fun  <- fun[[1]]
    if ( !(is.function(fun))) stop("Object: ", x, " in: ", where, " is NOT a function")
+   if (is.null(attr(fun, "original_body"))) attr(fun, "original_body") <- body(fun)  # save original body 
    return(fun)
 }
  
