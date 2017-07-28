b71c2e0d95a7df32d94aff59cc81f2d6979f4364
agalecki <agalecki@umich.edu>
2017-07-28 11:27:18 -0400

    Update and rename banno_examples.R to bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -1,4 +1,4 @@
-bfanno_simple <- function(x,  where =".GlobalEnv", idx = 0){
+bfanno_simple <- function(x,  where = ".GlobalEnv", idx = 0){
 # x is a character string containing function name
   getx   <- getAnywhere(x)
   whrAny <- getx[["where"]]
@@ -11,7 +11,7 @@ bfanno_simple <- function(x,  where =".GlobalEnv", idx = 0){
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
   if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
   msg1 <- substitute(message("-- Function [", idx, ":", x, "] from  [", where, "]"), list(idx = idx, x=x, where = where))
-  msg2 <- substitute(message("-- bannotator [banno_simple] used"))
+  msg2 <- substitute(message("-- bfannotator [bfanno_simple] used"))
   bf <- as.call(c(as.name("{"), msg1, msg2, b_f))
   return(bf) 
 }
