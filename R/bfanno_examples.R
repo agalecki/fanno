c0fbcdb002dd33d1dcf9273305272c829116b53e
agalecki <agalecki@umich.edu>
2017-07-28 13:32:44 -0400

    Update bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -1,11 +1,7 @@
 bfanno_simple <- function(x,  where = ".GlobalEnv", idx = 0){
 # x is a character string containing function name
-  fun <- fanno::extract_FUN(x, where = where)
-  if (is.null(attr(fun, "original_body"))) {
-     b_f <- body(fun)
-    } else {
-     b_f <- attr(fun, "original_body")
-    }
+  fun <- fanno::preprocess_FUN(x, where = where)
+  b_f <- attr(fun, "original_body")
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
   if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
   msg1 <- substitute(message("-- Function [", idx, ":", x, "] from  [", where, "]"), list(idx = idx, x=x, where = where))
