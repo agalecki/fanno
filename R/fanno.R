e525a4f119731fe4ae1174697faa4b78f325ae7b
agalecki <agalecki@umich.edu>
2017-07-28 16:26:34 -0400

    Update fanno.R

--- R/fanno.R
+++ R/fanno.R
@@ -1,19 +1,3 @@
- preprocess_FUN <- function(x, where = ".GlobalEnv") {
-   # x is a character string containing function name
-   getx   <- getAnywhere(x)
-   whrAny <- getx[["where"]]
-   len <- length(nx <- grep(where, whrAny))
-   if (len == 0)  stop("Function: ", x, " not found in: ", where)
-   fun  <- getx[["objs"]][nx]
-   fun  <- fun[[1]] 
-   if (!(is.function(fun))) stop("Object: ", x, " in: ", where, " is NOT a function")
-   if (is.null(attr(fun, "original_body"))) {
-      attr(fun, "original_body") <- body(fun)  # save original body 
-      } else {
-      body(fun)  <- attr(fun, "original_body")
-    }
-    return(fun)
-}
  
 assign_fanno <- function(x, where = ".GlobalEnv", idx = 0, bfanno = "bfanno_simple"){
  # x is a character string containing function name
