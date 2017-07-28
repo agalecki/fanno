2b66db78e2fff0c7300c1bdfd56ecf872fbf0927
agalecki <agalecki@umich.edu>
2017-07-28 14:36:47 -0400

    Update bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -1,5 +1,6 @@
 bfanno_simple <- function(fun,  flbl = "Fun", idx = 0){
 # x is a character string containing function name
+  if (!is.function(fun)) stop("fun needs to be a function")
   bfx <- attr(fun, "original_body")
   if (is.null(bfx))  b_f <- body(fun) else b_f <- bfx 
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
