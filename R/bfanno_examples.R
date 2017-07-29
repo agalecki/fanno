d7c78981d74a200c97e340c6f77e49443f55c706
agalecki <agalecki@umich.edu>
2017-07-29 13:14:15 -0400

    Update bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -1,8 +1,8 @@
-bfanno_simple <- function(fun,  flbl = "Fun", idx = 0){
+bfanno_msg1 <- function(fun,  flbl = "_flbl_", idx = 0){
 # x is a character string containing function name
-  if (!is.function(fun)) stop("fun needs to be a function")
-  bfx <- attr(fun, "original_body")
-  if (is.null(bfx))  b_f <- body(fun) else b_f <- bfx 
+  if (!is.function(fun)) stop("Arg fun needs to be a function")
+  bfx <- attr(fun, "original_fun")
+  if (is.null(bfx))  b_f <- body(fun) else b_f <- body(bfx) 
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
   if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
   msg1 <- substitute(message("-- Function [", idx, ":", flbl, "]"), list(idx = idx, flbl = flbl))
