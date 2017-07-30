3760e891d6b29fb3b869434d80b161397ffee728
agalecki <agalecki@umich.edu>
2017-07-30 15:07:37 -0400

    Update fanno.R

--- R/fanno.R
+++ R/fanno.R
@@ -1,5 +1,12 @@
- 
-assign_fanno <- function(x, where = ".GlobalEnv", idx = 0, bfanno = "bfanno_msg1"){
+update_finfo <- function(finfo, update = NULL) {
+# finfo is a named list with components: flbl  
+if (is.null(update))  update <- list(flbl = "ourfname", idx = 0, wher1 = ".GlobalEnv?", wher2 = "", bfanno = "bfanno_?")
+nmsu <- names(update)
+finfo[nmsu] <- update
+return(finfo)
+}
+
+assign_fanno <- function(x, where = ".GlobalEnv", bfanno = "bfanno_msg1"){
  # x is a character string containing function name
    getx   <- NULL
    getx   <- getAnywhere(x)
@@ -9,8 +16,15 @@ assign_fanno <- function(x, where = ".GlobalEnv", idx = 0, bfanno = "bfanno_msg1
    fun  <- getx[nx]                      # fun may have some attributes             
    if (!(is.function(fun))) stop("Object: ", x, " in: ", where, " is NOT a function")
    
- # In preparation for bfanno
- # store original function both in fun and in original_fun attribute  
+ # In preparation for bfanno: update finfo
+ finfo <- attr(fun, "finfo")
+ whr1 <- stringr::word(where, 1, sep = ":")
+ whr2 <- stringr::word(where, 2, sep = ":")
+ if (is.na(whr2)) whr2 <- ""
+ fupdt <- list(flbl = x, idx = idx, wher1 = whr1, wher2 = whr2, bfanno = bfanno)
+ finfo <- update_finfo(finfo, fupdt)
+ # store original function both in fun and in original_fun attribute
+ 
    if (is.null(attr(fun, "original_fun"))) {
       attr(fun, "original_fun") <- fun  
    } else {
@@ -19,11 +33,13 @@ assign_fanno <- function(x, where = ".GlobalEnv", idx = 0, bfanno = "bfanno_msg1
   fattr <- attributes(fun)
   ofun <- fun 
   attributes(ofun) <- NULL
-  bfanno_body <- do.call(bfanno, list(fun = fun, flbl = x, idx = idx)) 
+  
+  bfanno_body <- do.call(bfanno, list(fun = fun)) 
   body(fun)  <- bfanno_body
   attr(fun, "original_fun") <- ofun
   attr(fun, "bfanno") <- bfanno
-  whr1 <- stringr::word(where, 1, sep = ":")
+  attr(fun, "finfo")  <- finfo
+  
   if (whr1 == "namespace") {
      ns <-  stringr::word(where, 2, sep = ":")
      unlockBinding(x,getNamespace(ns))  
