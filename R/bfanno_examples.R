bfc81b9ff89c80db5cfff3958e3333c5ce34c939
agalecki <agalecki@umich.edu>
2017-07-31 15:52:10 -0400

    Update bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -1,13 +1,12 @@
 bfanno_msg1 <- function(fun){
   if (!is.function(fun)) stop("Arg fun needs to be a function")
-  finfo <- attr(fun, "finfo")           # Attribute containing finfo list 
-  finfo  <- update_finfo (finfo, finfo) 
   bfx <- attr(fun, "original_fun")
   if (is.null(bfx))  b_f <- body(fun) else b_f <- body(bfx) 
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
   if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
-  
-  msg1 <- substitute(message("-- Function [", idx, ":", flbl, "]",
+  finfo <- attr(fun, "finfo")           # Attribute containing finfo list 
+  finfo <- update_finfo (finfo, finfo) 
+  msg1  <- substitute(message("-- Function [", idx, ":", flbl, "]",
                              " from [",  where, "]",
                              " annotated using [", bfanno, "]"
                             ), finfo)
@@ -18,7 +17,7 @@ bfanno_msg1 <- function(fun){
 bfanno_strip <- function(fun){
 # returns original function body 
   if (!is.function(fun)) stop("Arg fun needs to be a function")
-  finfo <- attr(fun, "finfo")           # Attribute containing finfo list
-  if (is.null(finfo)) bfx <- body(fun) else bfx <- attr(fun, "original_fun") 
+  finfo <- attr(fun, "finfo")           # Attribute containing finfo list 
+  bfx <- if (is.null(finfo)) body(fun) else attr(fun, "original_fun") 
   return(bfx) 
 }
