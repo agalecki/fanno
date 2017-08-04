2319a87054323f4f8a513a580881ee029398a882
agalecki <agalecki@umich.edu>
2017-08-04 10:09:47 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -4,13 +4,8 @@
     fanno.finfo    = list( flbl     = "test_flbl",
                            where    = ".GlobalEnv",
                            idx      = 0,
-                           bfanno   = "bfanno_default",
-                           preamble =  function(finfo){
-                                         expr <- substitute(message("-- Function <", idx, ":", flbl, ">",
-                                         " from [",  where, "]",
-                                         " annotated using [", bfanno, "]"), finfo)
-                           return(as.expression(expr))
-                     }))
+                           bfanno   = "bfanno_default")
+                 )
   toset <- !(names(op.fanno) %in% names(op))
   if(any(toset)) options(op.fanno[toset])
   invisible()
