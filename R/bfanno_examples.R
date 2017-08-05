1fd991f963439f5939664d018aa2f0f991f922b0
agalecki <agalecki@umich.edu>
2017-08-05 11:18:13 -0400

    Update bfanno_examples.R

--- R/bfanno_examples.R
+++ R/bfanno_examples.R
@@ -22,17 +22,48 @@ ebody_default <- function(b_f, finfo){
  return(expr)
 }
 
-bfanno_default <- function(fun){
-  if (!isFun(fun)) stop("Arg fun of ineligible class",  class(fun)[1])
+bfanno_init <- function(fun){
+   if (!isFun(fun)) stop("Arg fun of ineligible class",  class(fun)[1])
+  
+  # Pre processing (typically no changes needed)
   bfx <- attr(fun, "original_fun")
   b_f <- if (is.null(bfx))  body(fun) else  body(bfx) 
   if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
   if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
-  finfo <- attr(fun, "finfo")             # Attribute containing finfo list 
-  finfo <- pad_finfo (finfo) 
-  epreamble <- epreamble_default(finfo)     #
-  eb_f    <- ebody_default(b_f, finfo)         
-  bf      <- as.call(c(as.name("{"), epreamble, eb_f))
+  b_f <- as.expression(b_f)           
+  finfo <- attr(fun, "finfo")               # Attribute containing finfo list 
+  finfo <- pad_finfo (finfo)                # pad finfo
+  res <- list(finfo = finfo, ebf = b_f)       
+return (res)
+}
+
+bfanno_default <- function(fun){
+# Pre - processing function: bf_list has two elements: finfo and ebf 
+   bfl   <-  bfanno_init(fun)
+   finfo <- bfl[["finfo"]]         # Padded finfo
+   ebf   <- bfl[["ebf"]]           # Expression representing body (fun)
+
+   # Prepare preamble expression using info stored in finfo
+   prex <- expression()
+   msg1 <- substitute(message("- Function <", idx, ":", flbl, ">",
+                              " from [",  where, "]",
+                              " annotated using [", bfanno, "]"), finfo)
+   traceR <- NULL                   
+   prex <- c(prex, msg1, traceR) 
+           
+   # expand body expression using info in ebf and finfo
+   
+   flblx <- paste(finfo$idx, finfo$flbl, sep=":")
+   bexpr <- expression()
+ 
+   for (i in seq_along(ebf)){
+     bi <- ebf[i]
+     bic <- as.character(bi)
+     ei <- substitute(message("   -  <", flblx, "> ln.", i, ":", bic), list(flbl = flbx, i = i, bic = bic)) 
+     bexpr <- c(bexpr, ei, bi)
+ }
+        
+  bf      <- as.call(c(as.name("{"), prex, bexpr))
   return(bf) 
 }
 
