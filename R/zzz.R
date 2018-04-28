efbc0083b75875147ecb0f86c43a458e54f7acf9
agalecki <agalecki@umich.edu>
2018-04-28 15:02:26 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   op <- options() 
-  op.fanno <- list(
-    fanno.finfo  = c("test_flbl",  ".GlobalEnv",  "bfanno_default"),    # flbl, where, bfanno
-    fanno.idx    = 0
-  )
+  op.fanno <- list( fanno.bfanno  = "bfanno_default")
   toset <- !(names(op.fanno) %in% names(op))
   if(any(toset)) options(op.fanno[toset])
   invisible()
