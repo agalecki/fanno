a86fe0e0344faab3b1b86d5961e6d1ba4e54e923
agalecki <agalecki@umich.edu>
2018-05-01 10:42:18 -0400

    Update createfinfo.R

--- R/createfinfo.R
+++ R/createfinfo.R
@@ -5,23 +5,27 @@ funinfoCreate <- function(fnm, where = ".GlobalEnv"){
 # funinfoCreate("word", where = "namespace:stringr")
 # funinfoCreate("word", where = "package:stringr")
  getFun <- getAnywhere(fnm)
+ funinfo <- list(
+     fnm   = fnm,
+     where = where)
  wherex    <- getFun[["where"]]
- if (!(where %in% wherex)) return(message("Object ", fnm, " not found in ", where)) 
+ 
  whr1 <- suppressMessages(stringr:::word(where, 1, sep =":"))
  whr2 <- suppressMessages(stringr:::word(where, 2, sep =":"))
  
  # Where position
  whrnms <- which(getFun[["where"]] == where)
- 
  is.found <-   if (length(whrnms) == 0) FALSE else TRUE  
  is.function <- TRUE
+ 
  # Extract object
  if (is.found) {
   lst1 <- getFun[["objs"]]
   fun <- getFun[["objs"]][[whrnms]]
   if (!is.function(fun)) is.function <- FALSE
-  if (!(c("function") %in% class(fun))) is.function <- FALSE
+if (!(c("function") %in% class(fun))) is.function <- FALSE
   if (!is.function) fun <- NULL
+  funinfo <- c(funinfo, found = is.found)
  } 
  
  # return expression containing body of an original function
@@ -30,20 +34,14 @@ funinfoCreate <- function(fnm, where = ".GlobalEnv"){
    b_f <- if (is.null(bfx))  body(fun) else  body(bfx) 
    if (b_f[[1]] == as.name("{") ) b_f[[1]] <- NULL
    ebf <- expression()
-   ebf <- c(ebf, b_f)         
- } else ebf <- NULL
- 
- funinfo <- list(
-     fnm   = fnm,
-     fun   = fun,
-     where = where,
-     ebf   = ebf,
-     found = is.found,
-     is.function = is.function)
- class(funinfo) <- "funinfo"
- 
- return(funinfo)
+   ebf <- c(ebf, b_f)
+   funinfo <- c(funinfo, is.function = is.function, fun =fun, ebf=ebf)
+    
+ }
+  class(funinfo) <- "funinfo"
+  return(funinfo)
 }
+
 fanno <- function(fnm, where = ".GlobalEnv", ebfanno= "ebfanno_simple", ...){
  # returns annotated function 
 
