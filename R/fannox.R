db4363356741c5148e3be1b22bd02e4dd19afe0b
agalecki <agalecki@umich.edu>
2018-05-03 15:48:01 -0400

    Update fannox.R

--- R/fannox.R
+++ R/fannox.R
@@ -1,8 +1,8 @@
-fanno <- function(fnm, where = ".GlobalEnv", aux = list(), ebfanno= "ebfanno_simple"){
+fanno <- function(fnm, where = ".GlobalEnv", aux = list(ebfanno= "ebfanno_simple", idx = 0)){
  # returns annotated function by invoking ebfanno function
- if (!is.character(fnm)) stop ("fnm needs to be  character")
+ # Returned function has additional attributes: fnm, where, original_fun, ebfanno
  funinfo <-  funinfoCreate(fnm, where = where)
- fun <- funinfo$fun 
+ fun <- funinfo$fun       
  ebf <- funinfo$orig_ebf
  origin_fun <- attr(fun, "original_fun")
  attrsfun <-attributes(fun)
@@ -10,10 +10,11 @@ fanno <- function(fnm, where = ".GlobalEnv", aux = list(), ebfanno= "ebfanno_sim
  
  # attrsfun[c("fnm", "where", "ebfanno")] <- NULL
  ff <- fun
- attributes(fun)  <- NULL
+ ## attributes(fun)  <- NULL
  ofun <- if (is.null(origin_fun)) fun else origin_fun 
  aofun <- if (is.null(origin_fun)) attributes(fun)  else  attributes(origin_fun)
- if (ebfanno == "ebfanno_strip" ) return(ofun)
+ attributes(ofun) <- aofun
+ if (ebfanno == "ebfanno_revert" ) return(ofun)
  argsl <- list(fnm = fnm, where = where, aux = aux)
  ebff <- do.call(ebfanno, argsl)
  bff  <- as.call(c(as.name("{"), ebff))
