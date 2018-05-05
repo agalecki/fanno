bed9eca766eb5cc73b4e250dd0f932dbf7b78eba
agalecki <agalecki@umich.edu>
2018-05-05 18:17:04 -0400

    Update fannox.R

--- R/fannox.R
+++ R/fannox.R
@@ -1,4 +1,41 @@
-fanno <- function(fnm, where = ".GlobalEnv", aux = list(ebfanno= "ebfanno_simple", idx = 0)){
+fanno <- function(fnm, where = ".GlobalEnv", efanno= "efanno_simple", aux = list()){
+if (!is.character(fnm)) stop("Argument fnm in fanno() needs to be a character")
+ if (!is.character(where) || !length(where) == 1 ) stop("invalid where argument in fanno()")
+ getFun <- getAnywhere(fnm)
+ wherex    <- getFun[["where"]]  # vector with one or more elements
+ whr1 <- suppressMessages(stringr:::word(where, 1, sep =":"))
+ whr2 <- suppressMessages(stringr:::word(where, 2, sep =":"))
+ 
+ # Where position
+ whrnms <- which(getFun[["where"]] == where)
+ is.found <-   if (length(whrnms) == 0) FALSE else TRUE
+ if (!(where %in% wherex)) is.found <- FALSE
+ 
+ # Extract object and creates is.function 
+ is.function <- is.found
+ fun <- NULL
+ if (is.found) {
+  lst1 <- getFun[["objs"]]
+  fun <- getFun[["objs"]][[whrnms]]
+  if (!is.function(fun)) is.function <- FALSE
+  if (!(c("function") %in% class(fun))) is.function <- FALSE
+  if (!is.function) fun <- NULL
+ } 
+ 
+ b_f <- body(fun)
+ ebf  <- if (is.null(b_f)) expression() else as.expression(b_f)
+ aux0 <- formals(efanno)   # Default arguments from annotating function
+ aux0$expr <- NULL
+ print(ebf)
+ res <- do.call(efanno, aux0)
+ return(res)
+ }
+
+#fanno("fx")
+
+
+
+fanno_deprecated <- function(fnm, where = ".GlobalEnv", aux = list(ebfanno= "ebfanno_simple", idx = 0)){
  # returns annotated function by invoking ebfanno function
  # Returned function has additional attributes: fnm, where, original_fun, ebfanno
  funinfo <-  funinfoCreate(fnm, where = where)
