a96a3561e72c42b2c823b8c54df8f1aa80a0d4a3
agalecki <agalecki@umich.edu>
2018-05-07 16:17:03 -0400

    Update fannomethods.R

--- R/fannomethods.R
+++ R/fannomethods.R
@@ -1,21 +1,10 @@
-fannotator_simple <- function(expr, aux = list(onm = "?expr?", where = "?where?")){
 
- ## Annotate  expression 
- e    <- expression()
- msg1 <- expression(message("Created on", Sys.time()))
- msg2 <- substitute(message("Function ", onm, " in ", where ," executed."), aux) 
- eanno <- c(e, msg1, msg2, expr)
- return(eanno)
-}
 
-#~~~~~~ 
 fanno <- function (x, ...) {
    UseMethod("fanno", x)
 }
 
-fanno.default <- function(x) x
-
-fanno.expression <- function(x, fannotator = "fannotator_simple", aux = list()){
+fanno.expression <- function(x, fannotator = "fannotator_simple", aux = list(nm ="?expr?")){
 # annotates object x of class expression
 # update aux0
 fannotated <-!is.null(attr(x, "fannotator"))
@@ -30,12 +19,13 @@ attr(exprx, "fannotator") <- fannotator
 return(exprx)
 }
 
-fanno.call <- function(x, fannotator = "eanno_simple", aux = list()){
+fanno.call <- function(x, fannotator = "fannotator_simple", aux = list(nm = "?call?")){
 # annotates object x of class call 
 fannotated <-!is.null(attr(x, "fannotator"))
 if (fannotated && fannotator == "fannotator_revert") return(attr(x,"original"))
 obf <- if (fannotated) attr(x,"original") else x
-oexpr <- as.expression(as.list(obf))
+oexpr_list <- as.list(obf)
+oexpr <- as.expression(oexpr_list)
 if (x[[1]] == as.name("{")) {
     x[[1]] <- NULL
     }
@@ -43,10 +33,26 @@ aux0 <- formals(get(fannotator))$aux
 if (length(names(aux))) aux0[names(aux)] <- aux
 args <- list(expr = oexpr, aux = aux0)
 exprx  <- do.call(fannotator, args)
-callx <- as.call(c(as.name("{"), exprx))
-attr(exprx, "original") <- obf
-attr(exprx, "fannotator") <- fannotator 
+callx <- as.call(as.list(exprx))
+attr(callx, "original") <- obf
+attr(callx, "fannotator") <- fannotator 
 return(callx)
 }
-## fanno.call(substitute({ y <- x^2; return(y) }))
-## fanno.call(substitute({ y <- x^2; return(y) }), aux = list(where="?where1"))
+
+fanno.function <- function(x, fannotator = "fannotator_simple", aux = list(nm="?fun?")){
+# annotates object x of class call 
+fannotated <-!is.null(attr(x, "fannotator"))
+if (fannotated && fannotator == "fannotator_revert") return(attr(x,"original"))
+ofun <- if (fannotated) attr(x,"original") else x
+obf <- body(ofun)
+oexpr <- as.expression(obf) # original expression
+aux0 <- formals(get(fannotator))$aux
+if (length(names(aux))) aux0[names(aux)] <- aux
+args <- list(expr = oexpr, aux = aux0)
+exprx  <- do.call(fannotator, args)
+funx <- x
+if (!is.null(obf)) body(funx) <- as.call(c(as.name("{"), exprx))
+attr(funx, "original") <- ofun
+attr(funx, "fannotator") <- fannotator 
+return(funx)
+}
