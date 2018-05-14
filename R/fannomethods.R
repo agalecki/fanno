e57c42743ceb96c5441b3429cf107c40a9f27169
agalecki <agalecki@umich.edu>
2018-05-14 14:39:04 -0400

    Update fannomethods.R

--- R/fannomethods.R
+++ R/fannomethods.R
@@ -39,20 +39,35 @@ attr(callx, "fannotator") <- fannotator
 return(callx)
 }
 
-fanno.function <- function(x, fannotator =  options()$fannotator, aux = list(flbl="?-fannno.function-?")){
-# annotates object x of class call 
-fannotated <-!is.null(attr(x, "fannotator"))
-## if (fannotated && fannotator == "fannotator_revert") return(attr(x,"original"))
-ofun <- if (fannotated) attr(x,"original") else x
+ #bf <- body(fun)                                # bf
+ #bcl <- coerce_bf_to_bcall(bf)                  # Creates (left) bracketed call
+ #exprvL <- coerce_bcall_to_exprvList(bcl)       # list with expressions
+ #bclx   <- coerce_expressionvList_to_bcall(exprvL) # Reconstructed bracketed call
+ #attributes(bclx) <- attributes(bcl)          
+
+
+fanno.function <- function(fun, fannotator =  options()$fannotator, aux = list(flbl="?-fannno.function-?")){
+# annotates object fun of class function 
+fannotated <-!is.null(attr(fun, "fannotator"))
+   
+# Extract original function
+ofun <- if (fannotated) attr(fun,"original") else fun
+   
+# Extract vector of one line expressions representing body of ofun
 obf <- body(ofun)
-oexpr <- as.expression(obf) # original expression
+obcl <- coerce_bf_to_bcall(obf)
+oexprvL <- coerce_bcall_to_exprvList(obcl)
+
+# prepare args for fannotator    
 aux0 <- formals(get(fannotator))$aux
 if (length(names(aux))) aux0[names(aux)] <- aux
-args <- list(expr = oexpr, aux = aux0)
-bf  <- do.call(fannotator, args) 
+args <- list(expr = oexprvL, aux = aux0)
 
-funx <- x
-if (!is.null(obf)) body(funx) <- bf
+oexprvL.new  <- do.call(fannotator, args) 
+bclx   <- coerce_expressionvList_to_bcall(oexprvL.new)
+attributes(bclx) <- attributes(obcl)
+funx <- fun
+if (!is.null(obf)) body(funx) <- bclx
 
 attr(funx, "original") <- ofun
 attr(funx, "fannotator") <- fannotator 
